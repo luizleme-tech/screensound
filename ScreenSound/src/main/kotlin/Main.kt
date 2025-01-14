30546559d7c096307d56f6e7994033811312178d
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-13 23:03:11 -0300

    refactor: movido de package

--- ScreenSound/src/main/kotlin/Main.kt
+++
@@ -1,5 +0,0 @@
-package com.luizlemetech
-
-fun main() {
-    println("Screen Sound")
-}
---
+++ ScreenSound/src/main/kotlin/com/luizlemetech/screensound/principal/Main.kt
@@ -0,0 +1,5 @@
+package com.luizlemetech.com.luizlemetech.screensound.principal
+
+fun main() {
+    println("Screen Sound")
+}
